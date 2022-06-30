470a3e7853a403662c822567022b72ee5fbde976
Ricardo Costa <ra.silva.dcosta@gmail.com>
2022-06-30 22:22:02 +0200

    creted the template index.html and modified the view index with a render shortcut

--- polls/views.py
+++ polls/views.py
@@ -4,9 +4,9 @@ from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join(q.question_text for q in latest_question_list)
+    context = {'latest_question_list': latest_question_list}
 
-    return HttpResponse(output)
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
     return HttpResponse('You\'re looking at questions %s.' % question_id)
